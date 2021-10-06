aec67ec7d0587a152e0ee3a7cafcc1c2094a860e
Tarini-27 <54857008+Tarini-27@users.noreply.github.com>
2021-10-06 21:35:47 +0530

    Update update.py

--- update.py
+++ update.py
@@ -14,10 +14,11 @@ def lambda_handler(event, context):
     print(path)
     for i in path:
         if i in fileNames_allowed:
-            filename = os.path.basename(i)
+            #filename = os.path.basename(i)
             #filename = filename.split('.')
             #filename = zipfile.Zipfile(filename[0], 'w', compression=ZIP_STORED)
-            s3.upload_file('filename', 'sunlife-cybersec-pe-freshers-backup', '2021/')
+            #s3.upload_file('filename', 'sunlife-cybersec-pe-freshers-backup', '2021/')
+            s3.put_object(Body='i',Bucket='sunlife-cybersec-pe-freshers-backup',Key='i')
             print("file uploaded to s3 successfully")
         else:
             print("not appropriate file modified")
