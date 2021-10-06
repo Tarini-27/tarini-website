b346a355e1b583db8e8faae72ec5721f6ce2a859
Tarini-27 <54857008+Tarini-27@users.noreply.github.com>
2021-10-06 16:01:00 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,28 +1,27 @@
 import os
 import boto3
 import yaml
+import zipfile
 
 cb = boto3.client('codebuild')
 s3 = boto3.client('s3')
-path=os.env('file_name')
-# with open(r'buildspec.yaml') as file:
-#     path = yaml.load(file, Loader=yaml.FullLoader)
-#     print(file_name)
+lb = boto3.client('lambda')
 
 def lambda_handler(event, context):
-
-    # Initialize needed variables
     file_extension_allowed = [".py"]
-    fileNames_allowed = ["lambda_1", "lambda_2"]
-    #region = event['Records'][0]['awsRegion']
+    fileNames_allowed = ["function", "function1"]
+    path=os.environ('file_name')
+    print(path)
+    for i in path:
+        if i in fileNames_allowed:
+            filename = os.path.basename(path)
+            filename = zipfile.Zipfile(filename, 'w', compression=ZIP_STORED)
+            s3.upload_file('filename', 'mybucket', 'test'))
+            print("file uploaded to s3 successfully")
 
-    # Check whether specific file or specific extension file is committed
-    trigger = False
-    file_name=os.path.basename(path)
-    root, extension = os.path.splitext(file_name)
-    if ((extension in file_extension_allowed) or (root in fileNames_allowed)):
-        trigger = True
 
-    if trigger:
-        s3.put_object(Bucket='sunlife-cybersec-pe-freshers-backup',Key='fileName')
+    
+            
+            
+
             
