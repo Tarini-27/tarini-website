9a3abae10bc079f1b7f8cfd9f76ac3b138d54e0c
Tarini-27 <54857008+Tarini-27@users.noreply.github.com>
2021-10-06 21:03:36 +0530

    Update update.py

--- update.py
+++ update.py
@@ -8,16 +8,16 @@ s3 = boto3.client('s3')
 lb = boto3.client('lambda')
 
 def lambda_handler(event, context):
-    file_extension_allowed = [".py"]
     fileNames_allowed = ["function.py", "function1.py"]
     path=os.environ('name1')
+    path=path.split(' ')
     print(path)
     for i in path:
         if i in fileNames_allowed:
             filename = os.path.basename(path)
             #filename = filename.split('.')
             #filename = zipfile.Zipfile(filename[0], 'w', compression=ZIP_STORED)
-            s3.upload_file('filename', 'sunlife-cybersec-pe-freshers-backup', 'filename')
+            s3.upload_file('filename', 'sunlife-cybersec-pe-freshers-backup', '2021/')
             print("file uploaded to s3 successfully")
         else:
             print("not appropriate file modified")
